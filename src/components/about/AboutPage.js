021b79ca2277e7a18ed6712f9ea738baa2270465
Yang <gyang@gagyang901.cvent.net>
2017-04-24 16:03:48 -0400

    initial changes

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class AboutPage extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>About</h1>
+                <p> about </p>
+            </div>        
+        );
+    }
+}
